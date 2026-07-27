645836c293b89d13752c3756e461fcb817e7348e
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-07-28 00:34:42 +0200

    Header and toggle button for to switch theme on my page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import React, { useState, createContext } from "react";
+import ReactSwitch from "react-switch";
 
+interface ThemeContextType {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+export const ThemeContext =  createContext<ThemeContextType | null>(null);
 
 function App() {
+  const [theme, setTheme] = useState("light");
 
+   const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+   };
   return (
-    <>
-    </>
+     <>
+     <ThemeContext.Provider value={{ theme, toggleTheme}}>
+       <div className={'main-container'} id={theme} >
+          <header className={'header-container'}>
+             <h1>Weather App</h1>
+             <ReactSwitch onChange={toggleTheme}
+                          checked={theme === "dark"}/>
+          </header>
+        </div>
+        </ThemeContext.Provider>
+     </>
+   
   )
 }
 
