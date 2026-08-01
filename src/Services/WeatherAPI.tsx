32738175c3b9463ed24bb230c0fc2352c327ea8e
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-08-01 23:14:01 +0200

    Updating my API service

--- src/Services/WeatherAPI.tsx
+++ src/Services/WeatherAPI.tsx
@@ -1,9 +1,25 @@
+import React from 'react'
 
 
 export const WeatherAPI = () => {
+  const fetchWeather = async () => {
+    const API_KEY = import.meta.env.VITE_APP_API_KEY;
+    const API_URL = import.meta.env.VITE_APP_API_URL;
+    const API_DEFAULT_CITY = import.meta.env.VITE_APP_DEFAULT_CITY;
+    const UNITS = import.meta.env.VITE_APP_UNITS;
+    const GEO_URL = import.meta.env.VITE_APP_GEO_URL;
+
+    const response = await fetch(
+      `${API_URL}/weather?q=${API_DEFAULT_CITY}&appid=${API_KEY}&unitS=${UNITS}`
+    );
+    const data = await response.json();
+    console.log(data);
+  }
   return (
       <>
-        
+        <button onClick={fetchWeather}>fetch weather</button>
       </>
   )
 }
+
+export default WeatherAPI
