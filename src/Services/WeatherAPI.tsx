f85a76fffda48c52bd646d4c3ee01f6398683b54
NeeloByron <byronnkhuna@gmail.com>
2026-08-04 11:11:04 +0200

    Functionality on WeatherAPI service component

--- src/Services/WeatherAPI.tsx
+++ src/Services/WeatherAPI.tsx
@@ -1,5 +1,4 @@
-
-import React, { createContext, useContext, useState, ReactNode } from 'react'
+import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react'
 
 interface WeatherData {
   name: string;
@@ -63,7 +62,6 @@ interface ForecastData {
     };
   }
 
-
 interface GeoCity {
   name: string;
   lat: number;
@@ -71,8 +69,10 @@ interface GeoCity {
   country: string;
   state?: string;
 }
+
 interface WeatherContextType {
   weather: WeatherData | null;
+  forecast: ForecastData | null;
   loading: boolean;
   error: string | null;
   fetchWeather: (city? : string) => Promise<void>;
@@ -86,17 +86,17 @@ interface WeatherContextType {
 
 export const WeatherAPI = ({ children }: { children: ReactNode}) => {
  const [weather, setWeather] = useState<WeatherData | null>(null);
- const [forecast, setForecast] = useState<WeatherData | null>(null);
+ const [forecast, setForecast] = useState<ForecastData | null>(null);
  const [loading, setLoading] = useState(false);
  const [error, setError] = useState<string | null>(null);
- 
-  const fetchWeather = async (city?: string) => {
+
     const API_KEY = import.meta.env.VITE_APP_API_KEY;
     const API_URL = import.meta.env.VITE_APP_API_URL;
-    const DEFAULT_CITY = import.meta.env.VITE_APP_DEFAULT_CITY;
+    const GEO_URL = import.meta.env.VITE_APP_GEO_URL;
+    const DEFAULT_CITY = import.meta.env.VITE_APP_DEFAULT_CITY || 'Polokwane';
     const UNITS = import.meta.env.VITE_APP_UNITS;
-  
-    
+ 
+  const fetchWeather = async (city?: string) => {
     {/*location*/}
     const location = city || DEFAULT_CITY;
     try {
@@ -130,42 +130,121 @@ export const WeatherAPI = ({ children }: { children: ReactNode}) => {
     }
   };
 
- /* const fetchForecast = async (city?: string) => {
+ const fetchForecast = async (city?: string) => {
     const location = city || DEFAULT_CITY;
 
     try {
       setLoading(true);
       setError(null);
-    }
-  }*/
 
-  const fetchGeolocation = async (city: string) => {
-    const GEO_URL = import.meta.env.VITE_APP_GEO_URL;
-    const API_KEY = import.meta.env.VITE_APP_API_KEY;
-
-    try {
-        const response = await fetch (
-          `${GEO_URL}/direct?q=${city}&limit=1&appid=${API_KEY}`
+      const response = await fetch (
+         `${API_URL}/forecast?q=${encodeURIComponent(location)}&appid=${API_KEY}&units=${UNITS}`
         );
-        const data = await response.json();
-        return data[0];
-    } catch (error) {
-      console.error('Error fetching coordinates:', error);
-      throw error;
+
+        if (!response.ok) {
+          if (response.status === 404) {
+            throw new Error(`City "${location}" not found. Please check spelling.`)
+          }
+          else if (response.status === 401) {
+            throw new Error('Invalid API key. Please check your configurations. ');
+          }
+          else {
+            throw new Error('Forecast service is temporary unavailable. Please try again later')
+          }
+        }
+
+        const data: ForecastData = await response.json();
+        setForecast(data);
+        console.log('Forecast data: ', data);
+
+     } catch (err) {
+       const errorMessage = err instanceof Error ? err.message : 'An unknown error occured';
+       setError(errorMessage);
+       console.error('Error:', err);
+     } finally {
+       setLoading(false);
+     }
+   };
+
+   const fetchWeatherByCoords = async (lat: number, lon: number) => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const response = await fetch (
+        `${API_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=${UNITS}`
+      );
+
+      if (!response.ok) {
+        if (response.status === 401) {
+          throw new Error('Invalid API key. Please check your configurations. ');
+        } else {
+          throw new Error('Weather service is temporarily unavailable. Please try again later.');
+        }
+      }
+      
+      const data: WeatherData = await response.json();
+      setWeather(data);
+      console.log('Weather by coords: ', data);
+
+    }  catch (err) {
+      const errorMessage = err instanceof Error ? err.message : 'An unknown error occured';
+      setError(errorMessage);
+      console.error('Error:', err);
+    } finally {
+      setLoading(false);
+     }
+   };
+
+   const searchCities = async (query: string): Promise<GeoCity[]> => {
+    try {
+      setError(null);
+
+      const response = await fetch (
+        `${GEO_URL}/direct?q=${encodeURIComponent(query)}&limit=5&appid=${API_KEY}`
+      );
+
+      if (!response.ok) {
+        if (response.status === 401) {
+          throw new Error('Invalid API key. Please check your configurations. ');
+        } else {
+          throw new Error('City search service is temporarily unavailable.')
+        }
+      }
+
+      const data: GeoCity[] = await response.json();
+      return data.map((city) => ({
+        name: city.name,
+        lat: city.lat,
+        lon: city.lon,
+        country: city.country,
+        state: city.state || "",
+      }));
+
+    } catch (err) {
+      const errorMessage = err instanceof Error ? err.message : 'An unknown error occured';
+      setError(errorMessage);
+      console.error('Error searching cities:', err);
+      return [];
     }
-  };
+   }
 
   const clearError = () => setError(null);
+   useEffect(() => {
+    fetchWeather();
+    fetchForecast();
+   }, []);
+
   return (
       <>
-        <WeatherContext.Provider value={{ weather, loading, error, fetchWeather, fetchGeolocation, clearError }}>
+        <WeatherContext.Provider value={{ weather, forecast, loading, error, fetchWeather, fetchForecast, searchCities, fetchWeatherByCoords, clearError }}>
           {children}
         </WeatherContext.Provider>
       </>
   );
 };
 
-export const useWeather = () => {
+export const useWeather = (): WeatherContextType => {
   const context = useContext(WeatherContext);
   if (context === undefined) {
     throw new Error('useWeather must be within a WeatherProvider');
